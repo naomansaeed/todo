6d99c7328feb9d414124ce68a39ac443794c7ce4
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-17 12:01:57 +0500

    feat: implemented the first working route /todos.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-// 1. Loads the built-in HTTP module
+//  Loads the built-in HTTP module
 const http = require('http');
 
-// 2. Defines the port 
+//  Defines the port 
 const PORT = 3000;
 
-// 3. Creates the server with a basic request handler
+// Creates the server with a basic request handler
 const server = http.createServer((req,res) => {
-    res.end('Hello from Node.js!');  // 4. Sends a simple response
+    //Set response header for JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    //Parse the URL to access pathname & query params
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    //Simple route: if GET request to /todos, send mock data
+    if (req.method === 'GET' && url.pathname === '/todos') {
+        res.statusCode = 200;
+        res.end(JSON.stringify({
+            message: 'GET /todos works!',
+            data: []
+        }));
+    }
+    else{
+        res.statusCode = 404;
+        res.end(JSON.stringify({
+            error: 'Route not found'
+        }));
+    }
+   // res.end('Hello from Node.js!');  //Sends a simple response
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
