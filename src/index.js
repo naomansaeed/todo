d2f78764f9a754baefce52b7a857edf12128fd1b
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-18 12:38:42 +0500

    feat: implemented basic push function.

--- src/index.js
+++ src/index.js
@@ -45,6 +45,45 @@ const server = http.createServer( async(req,res) => {
             endpoints: {'GET /todos': 'List all todos'}
         }));
     }
+
+    else if (req.method === 'POST' && url.pathname === '/todos') {
+        let body = '';
+        req.on('data', chunk => body += chunk);
+        req.on('end', async () => {
+        try {
+
+            //saves the recieved data
+            const newTodo = JSON.parse(body);
+
+            // fail if no title
+                if (!newTodo.title){
+                    throw new Error('Title is required');
+                } 
+
+            // read file contents asynchronously
+            const todos = await getTodos();
+
+            //generates id from date and saves as string value (adhoc way)
+            newTodo.id = Date.now().toString();
+
+            // appends to the in memory array
+            todos.push(newTodo);
+
+            // writes entire array to the file. null,2 are for indentation
+            await fs.writeFile(DATA_FILE, JSON.stringify(todos, null, 2));
+            
+            //sends status code on success
+            res.statusCode = 201;
+            
+            // sends the newly created data.
+            res.end(JSON.stringify({ message: 'Todo created', data: newTodo }));
+            } catch (err) {
+                //for handling error
+               res.statusCode = 400;
+               res.end(JSON.stringify({ error: err.message }));
+    }
+  });
+}
     else{
         res.statusCode = 404;
         res.end(JSON.stringify({
